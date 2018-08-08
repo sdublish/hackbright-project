7b66e9a953b1dcb60732f53a60a80a91b5d24c27
Supriya Dublish <sdublish16@gmail.com>
2018-08-08 22:13:37 +0000

    Added loading icon for GR following

--- static/js/authorInfo.js
+++ static/js/authorInfo.js
@@ -8,3 +8,8 @@ $("#fav-author").on("submit", function(evt){
         alert(results.result);
     });
 });
+
+// on clicking the Goodreads favorite button, shows a spinner to indicate stuff is happening behind the scenes
+$("#gr-fav-btn").on("click", function(){
+    $("#gr-fav-btn").html("<i class='fas fa-spinner fa-spin'></i>");
+})
